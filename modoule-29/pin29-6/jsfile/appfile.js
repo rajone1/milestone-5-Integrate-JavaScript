51bd98f8fdb9861ba62ac646848d7b808da35b04
rajone1 <rajoneweab101@gmail.com>
2023-11-20 21:41:24 +0600

    module29

---
+++ modoule-29/pin29-6/jsfile/appfile.js
@@ -0,0 +1,29 @@
+function getpin() {
+  const pin = generatepin();
+
+  const pinstring = pin + "";
+
+  if (pinstring.length === 4) {
+    return pin;
+  } else {
+    //     console.log("pin is lessthan 4", pin);
+    return getpin();
+  }
+}
+
+function generatepin() {
+  const genarate = Math.round(Math.random() * 10000);
+  return genarate;
+}
+// add pin button
+document.getElementById("generate-pin").addEventListener("click", function () {
+  const pinbutton = getpin();
+  //   console.log(pinbutton);
+  // set pin display
+  const inputpindisplay = document.getElementById("pin-input");
+
+  inputpindisplay.value = pinbutton;
+});
+
+// generatepin();
+// console.log(generatepin());

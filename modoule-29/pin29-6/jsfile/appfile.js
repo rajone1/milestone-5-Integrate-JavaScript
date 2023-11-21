d2ca551b26394224a25e23f57bc4c728f8418816
rajone1 <rajoneweab101@gmail.com>
2023-11-21 21:00:07 +0600

    new1

--- modoule-29/pin29-6/jsfile/appfile.js
+++ modoule-29/pin29-6/jsfile/appfile.js
@@ -1,3 +1,5 @@
+// 1
+
 function getpin() {
   const pin = generatepin();
 
@@ -10,12 +12,12 @@ function getpin() {
     return getpin();
   }
 }
-
+// 2
 function generatepin() {
   const genarate = Math.round(Math.random() * 10000);
   return genarate;
 }
-// add pin button
+// 3 add pin button
 document.getElementById("generate-pin").addEventListener("click", function () {
   const pinbutton = getpin();
   //   console.log(pinbutton);
@@ -28,10 +30,42 @@ document.getElementById("generate-pin").addEventListener("click", function () {
 // generatepin();
 // console.log(generatepin());
 
-// calculator part
+// 4 calculator part
 
 document
   .getElementById("calculator")
   .addEventListener("click", function (event) {
-    console.log(event.target.innerText);
+    const numbr = event.target.innerText;
+    const getinput = document.getElementById("typed-numbers");
+    if (isNaN(numbr)) {
+      if (numbr === "C") {
+        getinput.value = "";
+      }
+    } else {
+      const previousinputvalue = getinput.value;
+
+      const newinputvalue = previousinputvalue + numbr;
+      getinput.value = newinputvalue;
+    }
   });
+// 5 submit button
+
+document.getElementById("submit").addEventListener("click", function () {
+  //
+  const pininput = document.getElementById("pin-input");
+  const pinpointvalue = pininput.value;
+  //
+  const typednumbers = document.getElementById("typed-numbers");
+
+  const typednumbersValue = typednumbers.value;
+
+  const pinPositive = document.getElementById("pin-positive");
+  const pinNegetive = document.getElementById("pin-negative");
+  if (pinpointvalue === typednumbersValue) {
+    pinPositive.style.display = "block";
+    pinNegetive.style.display = "none";
+  } else {
+    pinNegetive.style.display = "block";
+    pinPositive.style.display = "none";
+  }
+});
